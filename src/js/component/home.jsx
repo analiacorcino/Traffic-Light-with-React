4bcecd6c1e723f5879ba889224acc59737ba3c4d
analiacorcino <analia.corcino@gmail.com>
2022-10-05 18:01:22 +0000

    Mi primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,47 @@
-import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useState} from "react";
+import '../../styles/index.css';
 
-//create your first component
+//CREAR COMPONENTE ! 
+
 const Home = () => {
+	const [selected1, setSelected1] = useState("sombra");
+	const [selected2, setSelected2] = useState("sombra");
+	const [selected3, setSelected3] = useState("sombra");
+
+	// Si es apretadop el color rojo
+	function PrendeRojo() {
+		if (selected1 === "sombra") {
+			setSelected1("brillo")
+			setSelected2("sombra")
+			setSelected3("sombra")
+		};
+	};
+
+	// Si es apretado el amarillo
+	function PrendeAmarillo() {
+		if (selected2 === "sombra") {
+			setSelected1("sombra")
+			setSelected2("brillo")
+			setSelected3("sombra")
+		};
+		};
+
+	// Si es apretado el verde
+	function PrendeVerde() {
+		if (selected3 === "sombra") {
+			setSelected1("sombra")
+			setSelected2("sombra")
+			setSelected3("brillo")
+		};
+	};
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container w-25 p-5 bg-dark d-flex justify-content-center">
+			<div>
+				<div className={"m-3 caja rounded-circle bg-danger "+selected1} onClick={PrendeRojo}></div>
+				<div className={"m-3 caja rounded-circle bg-warning "+selected2} onClick={PrendeAmarillo}></div>
+				<div className={"m-3 caja rounded-circle bg-success "+selected3} onClick={PrendeVerde}></div>
+			</div>
 		</div>
 	);
 };
